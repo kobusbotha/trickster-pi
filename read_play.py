35d2159962b0d0d04aad23d82553228a39699c6d
kommetjie <you@example.com>
2014-04-24 20:46:51 +0000

    wav pack 1

--- read_play.py
+++ read_play.py
@@ -10,22 +10,36 @@ import array
 import time
 import threading
 import serial
+import os
+import os.path
 
-sounds = [0 for j in range(0,16)]
-stubbed_data = [0, 1, 0, 1];
+sounds = {}
+wave_set = 0
+stubbed_data = [0, 1, 0, 1, 1];
+wav_delay = 0.8
 try: 
 	stubbed = False;
 	ser = Serial('/dev/ttyACM0', 9600)
 except serial.serialutil.SerialException:
 	stubbed = True;
 	
-data = [0 for j in range(0,4)]
+data = [0 for j in range(0,5)]
 
 #Initialize Pygame
 def init():
 	def sound_init():
-		for i in range(0,16):
-			sounds[i] = pygame.mixer.Sound("/home/pi/trickster-pi/wav" + str(i) + ".wav")
+		k = 0
+		path = "/home/pi/trickster-pi/wav/"
+		for x in os.listdir(path):
+			if os.path.isdir(os.path.join(path, x)):
+				waves = []
+				for i in range(4):
+					print os.path.join(path, x,  str(i) + ".wav")	
+					waves.append(pygame.mixer.Sound(os.path.join(path, x,  str(i) + ".wav")))
+				sounds[k] = waves
+				k += 1
+		print sounds
+
 	pygame.init()
 	pygame.mixer.init()
 	sound_init()
@@ -33,14 +47,27 @@ def init():
 
 def playall():
 	global data
+	global wave_set
 	if stubbed:
 		data = stubbed_data
 	while 1:
-		for i in range(0, 4):
-			if data[i] != 0:
-				sounds[data[i]].play()
-			time.sleep(0.4)
-
+		command = data[4]
+		print "cmd", command, "set", wave_set
+		if command != 1:
+			for i in range(0, 4):
+				if data[i] != 0:
+					binary = "{0:b}".format(data[i]).rjust(4,"0")
+					print data[i], binary
+					if binary[0] == '1':
+						sounds[wave_set][0].play()
+                                        if binary[1] == '1':
+                                                sounds[wave_set][1].play()
+                                        if binary[2] == '1':
+                                                sounds[wave_set][2].play()
+                                        if binary[3] == '1':
+                                                sounds[wave_set][3].play()
+				time.sleep(sounds[wave_set][0].get_length()/2)
+	
 def coroutine(func):
     def start(*args,**kwargs):
 	cr = func(*args,**kwargs)
@@ -83,14 +110,19 @@ def unwrap_protocol(header='\x61',
 @coroutine
 def frame_receiver():
 	global data
+	global wave_set
 	while True:
 		frame = (yield)
 		print 'Got frame:', frame.encode('hex')
 		print 'Length:', len(frame)
-		if len(frame) == 4:
-			for i in range(0,4):
-				data[i]=int(frame[i].encode('hex')[1])
+		if len(frame) == 5:
+			for i in range(0,5):
+				data[i]=int(frame[i].encode('hex')[1], 16)
 				print '%s : %s' % (i,data[i])
+			if data[4] == 2:
+                        	wave_set = (wave_set + 1) % len(sounds)
+                        elif data[4] == 3:
+                                wave_set = (wave_set - 1) % len(sounds)
 		else:
 			print "Wrong Length"
 
